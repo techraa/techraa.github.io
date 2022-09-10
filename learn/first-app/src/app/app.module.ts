e028b8b16070beda4112b1d4fb495331de432066
mudda <13121582+swathimudda@users.noreply.github.com>
2022-09-10 10:58:42 -0500

    ...

--- learn/first-app/src/app/app.module.ts
+++ learn/first-app/src/app/app.module.ts
@@ -1,21 +1,15 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
